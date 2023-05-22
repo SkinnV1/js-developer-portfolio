794b1a3c6f9d2fa2136c02d303202d53e1b5c7d2
SkinnV1 <thatcisio77@gmail.com>
2023-05-22 17:30:53 -0300

    update language e portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,32 @@ function updateProfileData(profileData) {
   email.href = 'mailto:${profileData.email}'
 }
 
-function updateSoftSkill(profileData) {
-  const softSkills = document.getElementById(`profile.skills.softskills`)
-  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+function updateSkill(profileData) {
+  const hardSkills = document.getElementById(`profile.skills.hardskills`)
+  hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li>
+  <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">
+  </li>`).join('')
+
+  const softSkills = document.getElementById('profile.skills.softskills')
+  softSkills.innerHTML = profileData.skills.softSkills.map(skill =>`<li>${skill}</li>`).join('')
+}
+
+function updateLanguage(profileData){
+  const language = document.getElementById('profile.languages')
+  language.innerHTML = profileData.languages.map( language => `<li>${language}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById('profile.portfolio')
+  portfolio.innerHTML = profileData.portfolio.map(project => `<li><h3 class="title">${project.name}</h3>
+  <a href="${project.url}" target="_blank" rel="noopener noreferrer">${project.url}</a></li>`).join('')
 }
 
 (async () =>{
   const profileData = await fethcProfileData()
   updateProfileData(profileData)
-  updateSoftSkill(profileData)
-
+  updateSkill(profileData)
+  updateLanguage(profileData)
+  updatePortfolio(profileData)
 
 })()
